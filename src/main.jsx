0af993a1243f2cdb08d2803c694b9bee2c7f85f6
Raushan Raj <raushanraj1205@gmail.com>
2024-11-07 04:01:39 +0530

    Reset nd Test started

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { BrowserRouter, RouterProvider, Routes, Route } from 'react-router-dom'; // Ensure these are imported
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css';
+import App from './App';
+import Contact from './Contact';
+import Aboutme from './Aboutme';
+const router = createBrowserRouter([
+  {
+    path: "/portfolio.io/",
+    element: <App />,
+  },
+  {
+    path: "/portfolio.io/contact/",
+    element: <Contact />,
+  },
+  {
+    path: "/portfolio.io/about/",
+    element: <Aboutme />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: '#ffffff',
+        purple: '#3f3cbb',
+        midnight: '#121063',
+        metal: '#565584',
+        tahiti: '#3ab7bf',
+        silver: '#ecebff',
+        'bubble-gum': '#ff77e9',
+        bermuda: '#78dcca',
+        // Add your custom colors here
+        gray: '#ABB2BF',
+        primary: '#C778DD',
+        background: '#282C33',
+      },
+      fontFamily: {
+        fira: ["Fira Code", "monospace"],
+      },
+    },
   },
   plugins: [],
-};
+}
