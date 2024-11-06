a8990d78cb6801ce576f2dd6c10db9a9cd2c23e8
Raushan Raj <raushanraj1205@gmail.com>
2024-11-07 04:09:17 +0530

    Reset nd Test started_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./index.css";
 function App() {
-  c;
   return (
     <>
       <p>Hello, I am here to test your Hosting</p>
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import { BrowserRouter, RouterProvider, Routes, Route } from 'react-router-dom'; // Ensure these are imported
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css';
-import App from './App';
-import Contact from './Contact';
-import Aboutme from './Aboutme';
-const router = createBrowserRouter([
-  {
-    path: "/portfolio.io/",
-    element: <App />,
-  },
-  {
-    path: "/portfolio.io/contact/",
-    element: <Contact />,
-  },
-  {
-    path: "/portfolio.io/about/",
-    element: <Aboutme />,
-  },
-]);
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client"; // Make sure to use the 'client' version if using React 18+
+import App from "./App"; // Your root component
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
