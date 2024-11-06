13c818f212211939e30321253332632885fddcbc
Raushan Raj <raushanraj1205@gmail.com>
2024-11-07 04:33:37 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
 );
